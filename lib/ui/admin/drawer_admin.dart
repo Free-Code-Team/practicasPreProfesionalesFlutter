257fe8a7e4101e6b5416dee4c977d469004edc35
JhobanyT <jhobanyticona@gmail.com>
2022-12-14 13:25:18 -0500

    drawer

--- lib/ui/admin/drawer_admin.dart
+++ lib/ui/admin/drawer_admin.dart
@@ -26,13 +26,29 @@ class DrawerAdmin extends StatelessWidget {
           builder: (context, state) {
             return ListView(
               padding: EdgeInsets.zero,
-              children: [
-                DrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
+              children: <widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text('Jhobany Ticona', style: (TextStyle(fontSize: 17)),),
+                  accountEmail: Text('Jhobany@gmail.com', style: (TextStyle(fontSize: 15)),),
+                  currentAccountPicture: CircleAvatar(
+                    child: ClipOval(
+                      child: Image.network(
+                        'https://media-exp1.licdn.com/dms/image/C4D03AQFQ4EsbEQOP9g/profile-displayphoto-shrink_200_200/0/1650294861122?e=1675900800&v=beta&t=WNC7VAtLDtJlf-dTeayN6zYP3EAQQsUBkSVmVwsNpIo',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover
+                      ),
+                    ),
                   ),
-                  child: Text('as'),
-                ),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                      'https://cloudraya.com/blog/wp-content/uploads/2021/10/Blog-Oct-3-1.jpg',
+                    ),
+                    fit: BoxFit.cover
+                    )
+                  ),
+                  ),
                 ListTile(
                   leading: const Icon(
                     Icons.home,
@@ -59,6 +75,17 @@ class DrawerAdmin extends StatelessWidget {
                   onTap: () {
                     Navigator.pushNamed(context, '/empresa_home');
                   },
+                ),
+                Container(     
+                  margin: const EdgeInsets.only(top: 150, bottom: 12, left: 12, right: 12),
+                  width: double.infinity,
+                  //color: Color.fromARGB(255, 0, 7, 109),
+                  child: Image.network(
+                        'https://upeu.edu.pe/wp-content/uploads/2022/03/04.png',
+                        width: 200,
+                        height: 200,
+                        fit: BoxFit.cover
+                      ),
                 ),
               ],
             );
